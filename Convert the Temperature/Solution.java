19ae6da558a0f60e8f5afa1ac3390b02f0696854
andliz1 <113252284+andliz1@users.noreply.github.com>
2023-01-29 11:50:04 +0100

    Create Solution.java

---
+++ Convert the Temperature/Solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public double[] convertTemperature(double celsius) 
+    {
+        double[] ans = {(celsius + 273.15), (celsius * 1.80 + 32.00)}; 
+
+        return ans;  
+    }
+}
