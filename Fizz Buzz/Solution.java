7d65a8f7de07611f8b07f76012f2f38e48427b45
andliz1 <113252284+andliz1@users.noreply.github.com>
2023-01-29 11:34:25 +0100

    Create Solution.java

---
+++ Fizz Buzz/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+
+        List<String> list = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++ ){
+            if (i % 15 == 0)
+                list.add("FizzBuzz");
+            else if (i % 5 == 0)
+                list.add("Buzz");
+            else if (i % 3 == 0)
+                list.add("Fizz");
+            else
+            {
+                String num = Integer.toString(i);
+                list.add(num);
+            }
+
+        }
+        return list;
+        
+    }
+}
